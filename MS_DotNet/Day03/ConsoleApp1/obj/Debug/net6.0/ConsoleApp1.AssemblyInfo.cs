291622c003a7c13e54006513b427506967a83114
PrathameshDeshpande6546 <prathameshdeshpande68@gmail.com>
2024-12-30 15:09:09 +0530

    CDAC Aug 2024

--- MS_DotNet/Day03/ConsoleApp1/obj/Debug/net6.0/ConsoleApp1.AssemblyInfo.cs
+++ MS_DotNet/Day03/ConsoleApp1/obj/Debug/net6.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+022a12e54f3cb87fce3360196e351dbf38678dd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2861b5dd58d4f9f0dd6aebd03e0b58a425f905f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
